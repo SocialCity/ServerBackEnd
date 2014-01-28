6bd354c3f6246c66a375deef33e81731c2b73af1
Fusian <fusian@Alucard.(none)>
2014-01-28 20:43:53 +0000

    Added all the new factors, everythings broken

--- 4YP-SocialCity/src/com/SocialCity/DataParsers/ExcelParsing.java
+++ 4YP-SocialCity/src/com/SocialCity/DataParsers/ExcelParsing.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import jxl.Sheet;
 import jxl.Workbook;
@@ -24,22 +25,30 @@ public class ExcelParsing {
 		MongoClient mongoClient;
 		Sheet sheet = null;
 		String housePrice;
-		DBCollection coll = null;
-		DBCollection collBorough = null;
+		ArrayList<DBCollection> collWard = new ArrayList<DBCollection>();
+		ArrayList<DBCollection> collBorough = new ArrayList<DBCollection>();
 		String name;
 		HashMap<String, Double> tweetCount = (new TweetByArea()).tweetProportion(null);
 		
 		try {//set up file reader and databases
-			Workbook workbook = Workbook.getWorkbook(new File("resources/ward-profiles-excel-version.xls"));
+			Workbook workbook = Workbook.getWorkbook(new File("resources/wardData.xls"));
 
-			sheet = workbook.getSheet(1); 
+			sheet = workbook.getSheet(5); 
 			mongoClient = new MongoClient("localhost");
 			mongoClient.dropDatabase("areas");
 			DB db = mongoClient.getDB( "areas" );
-			db.createCollection("wards", null);
-			coll = db.getCollection("wards");
-			db.createCollection("boroughs", null);
-			collBorough = db.getCollection("boroughs");
+			
+			collWard.add(db.createCollection("wards2008", null));
+			collWard.add(db.createCollection("wards2009", null));
+			collWard.add(db.createCollection("wards2010", null));
+			collWard.add(db.createCollection("wards2011", null));
+			collWard.add(db.createCollection("wards2012", null));
+			
+			collBorough.add(db.createCollection("boroughs2008", null));
+			collBorough.add(db.createCollection("boroughs2009", null));
+			collBorough.add(db.createCollection("boroughs2010", null));
+			collBorough.add(db.createCollection("boroughs2011", null));
+			collBorough.add(db.createCollection("boroughs2012", null));
 			
 		} catch (BiffException e) {
 			e.printStackTrace();
@@ -47,41 +56,99 @@ public class ExcelParsing {
 			e.printStackTrace();
 		} 
 		
-		for (int i=1; i<sheet.getRows()-3;i++) {//gets needed factors for every location
-			String code = sheet.getCell(1, i).getContents();
-			SocialFactors sF = new SocialFactors(code);
+		for (int i=3; i<627;i++) {//gets needed factors for every location
+			String code = sheet.getCell(0, i).getContents();
+			ArrayList<SocialFactors> sF = new ArrayList<SocialFactors>();
 			
-			sF.setCrimeRate(Double.parseDouble(sheet.getCell(55, i).getContents()));
-			name = sheet.getCell(0, i).getContents();
-			if (name.contains("-")) {
-				name = name.substring(name.indexOf("-") + 2);
+			sF.add(new SocialFactors(code));
+			sF.add(new SocialFactors(code));
+			sF.add(new SocialFactors(code));
+			sF.add(new SocialFactors(code));
+			sF.add(new SocialFactors(code));
+			
+			sF.get(1).setIncapacityBenefit(Double.parseDouble(sheet.getCell(9, i).getContents()));
+			sF.get(2).setIncapacityBenefit(Double.parseDouble(sheet.getCell(10, i).getContents()));
+			sF.get(3).setIncapacityBenefit(Double.parseDouble(sheet.getCell(11, i).getContents()));
+			sF.get(4).setIncapacityBenefit(Double.parseDouble(sheet.getCell(12, i).getContents()));
+			sF.get(5).setIncapacityBenefit(Double.parseDouble(sheet.getCell(13, i).getContents()));
+			
+			sF.get(1).setUnemploymentRate(Double.parseDouble(sheet.getCell(14, i).getContents()));
+			sF.get(2).setUnemploymentRate(Double.parseDouble(sheet.getCell(15, i).getContents()));
+			sF.get(3).setUnemploymentRate(Double.parseDouble(sheet.getCell(16, i).getContents()));
+			sF.get(4).setUnemploymentRate(Double.parseDouble(sheet.getCell(17, i).getContents()));
+			sF.get(5).setUnemploymentRate(Double.parseDouble(sheet.getCell(18, i).getContents()));
+			
+			sF.get(1).setIncomeSupport(Double.parseDouble(sheet.getCell(19, i).getContents()));
+			sF.get(2).setIncomeSupport(Double.parseDouble(sheet.getCell(20, i).getContents()));
+			sF.get(3).setIncomeSupport(Double.parseDouble(sheet.getCell(21, i).getContents()));
+			sF.get(4).setIncomeSupport(Double.parseDouble(sheet.getCell(22, i).getContents()));
+			sF.get(5).setIncomeSupport(Double.parseDouble(sheet.getCell(23, i).getContents()));
+			
+			sF.get(1).setCrimeRate(Double.parseDouble(sheet.getCell(24, i).getContents()));
+			sF.get(2).setCrimeRate(Double.parseDouble(sheet.getCell(25, i).getContents()));
+			sF.get(3).setCrimeRate(Double.parseDouble(sheet.getCell(26, i).getContents()));
+			sF.get(4).setCrimeRate(Double.parseDouble(sheet.getCell(27, i).getContents()));
+			sF.get(5).setCrimeRate(Double.parseDouble(sheet.getCell(28, i).getContents()));
+
+			sF.get(1).setDeliberateFires(Double.parseDouble(sheet.getCell(29, i).getContents()));
+			sF.get(2).setDeliberateFires(Double.parseDouble(sheet.getCell(30, i).getContents()));
+			sF.get(3).setDeliberateFires(Double.parseDouble(sheet.getCell(31, i).getContents()));
+			sF.get(4).setDeliberateFires(Double.parseDouble(sheet.getCell(32, i).getContents()));
+			sF.get(5).setDeliberateFires(Double.parseDouble(sheet.getCell(33, i).getContents()));
+			
+			sF.get(1).setGCSEScore(Double.parseDouble(sheet.getCell(34, i).getContents()));
+			sF.get(2).setGCSEScore(Double.parseDouble(sheet.getCell(35, i).getContents()));
+			sF.get(3).setGCSEScore(Double.parseDouble(sheet.getCell(36, i).getContents()));
+			sF.get(4).setGCSEScore(Double.parseDouble(sheet.getCell(37, i).getContents()));
+			sF.get(5).setGCSEScore(Double.parseDouble(sheet.getCell(38, i).getContents()));
+			
+			sF.get(1).setSchoolAbscences(Double.parseDouble(sheet.getCell(39, i).getContents()));
+			sF.get(2).setSchoolAbscences(Double.parseDouble(sheet.getCell(40, i).getContents()));
+			sF.get(3).setSchoolAbscences(Double.parseDouble(sheet.getCell(41, i).getContents()));
+			sF.get(4).setSchoolAbscences(Double.parseDouble(sheet.getCell(42, i).getContents()));
+			sF.get(5).setSchoolAbscences(Double.parseDouble(sheet.getCell(43, i).getContents()));
+
+			sF.get(1).setChildInNoWorkHouse(Double.parseDouble(sheet.getCell(44, i).getContents()));
+			sF.get(2).setChildInNoWorkHouse(Double.parseDouble(sheet.getCell(45, i).getContents()));
+			sF.get(3).setChildInNoWorkHouse(Double.parseDouble(sheet.getCell(46, i).getContents()));
+			sF.get(4).setChildInNoWorkHouse(Double.parseDouble(sheet.getCell(47, i).getContents()));
+			sF.get(5).setChildInNoWorkHouse(Double.parseDouble(sheet.getCell(48, i).getContents()));
+			
+			sF.get(1).setTransportRating(Double.parseDouble(sheet.getCell(49, i).getContents()));
+			sF.get(2).setTransportRating(Double.parseDouble(sheet.getCell(50, i).getContents()));
+			sF.get(3).setTransportRating(Double.parseDouble(sheet.getCell(51, i).getContents()));
+			sF.get(4).setTransportRating(Double.parseDouble(sheet.getCell(52, i).getContents()));
+			sF.get(5).setTransportRating(Double.parseDouble(sheet.getCell(53, i).getContents()));
+			
+			if (sF.get(1).getLocation().get(0).length() == 6) {//checks if location is a ward
+				collWard.get(1).insert(sF.get(1).getDBObject());
+				collWard.get(2).insert(sF.get(2).getDBObject());
+				collWard.get(3).insert(sF.get(3).getDBObject());
+				collWard.get(4).insert(sF.get(4).getDBObject());
+				collWard.get(5).insert(sF.get(5).getDBObject());
 			}
-			System.out.println(name);
-			housePrice = (sheet.getCell(26,i).getContents()).substring(3);
-			housePrice = housePrice.replace(",", "");
-			sF.setHousePrice(Double.parseDouble(housePrice));
-			
-			sF.setEducationRating(Double.parseDouble(sheet.getCell(54, i).getContents()));
-			
-			sF.setTransportRating(Double.parseDouble(sheet.getCell(64, i).getContents()));
-			
-			sF.setMeanAge(Double.parseDouble(sheet.getCell(9, i).getContents()));
-			sF.setDrugRate(Double.parseDouble(sheet.getCell(60, i).getContents()));
-			sF.setEmploymentRate(Double.parseDouble(sheet.getCell(22, i).getContents()));
-			sF.setVoteTurnout(Double.parseDouble(sheet.getCell(65, i).getContents()));
-			
-			if (sF.getLocation().get(0).length() == 6) {//checks if location is a ward
-				coll.insert(sF.getDBObject());
+			else if (sF.get(1).getLocation().get(0).equals("00AA")) {//city of london borough must be both ward and borough
+				collWard.get(1).insert(sF.get(1).getDBObject());
+				collWard.get(2).insert(sF.get(2).getDBObject());
+				collWard.get(3).insert(sF.get(3).getDBObject());
+				collWard.get(4).insert(sF.get(4).getDBObject());
+				collWard.get(5).insert(sF.get(5).getDBObject());
+				
+				collBorough.get(1).insert(sF.get(1).getDBObject());
+				collBorough.get(2).insert(sF.get(2).getDBObject());
+				collBorough.get(3).insert(sF.get(3).getDBObject());
+				collBorough.get(4).insert(sF.get(4).getDBObject());
+				collBorough.get(5).insert(sF.get(5).getDBObject());
 			}
-			else if (sF.getLocation().get(0).equals("00AA")) {//city of london borough must be both ward and borough
-				coll.insert(sF.getDBObject());
-				collBorough.insert(sF.getDBObject());
-			}
-			else if (sF.getLocation().get(0).length() == 4){//checks if locale is a borough
-				collBorough.insert(sF.getDBObject());
-				//System.out.println("heyo");
+			else if (sF.get(1).getLocation().get(0).length() == 4){//checks if locale is a borough
+				collBorough.get(1).insert(sF.get(1).getDBObject());
+				collBorough.get(2).insert(sF.get(2).getDBObject());
+				collBorough.get(3).insert(sF.get(3).getDBObject());
+				collBorough.get(4).insert(sF.get(4).getDBObject());
+				collBorough.get(5).insert(sF.get(5).getDBObject());
 			}
 			//System.out.println(sF.getDBObject());
+			sF.clear();
 		}
 
 	}
--- 4YP-SocialCity/src/com/SocialCity/SocialFactor/SocialFactors.java
+++ 4YP-SocialCity/src/com/SocialCity/SocialFactor/SocialFactors.java
@@ -12,15 +12,18 @@ public class SocialFactors {
 	private ArrayList<String> location;
 	private double crimeRate;
 	private double housePrice;
-	private double educationRating;
 	private double transportRating;
-	
-	private double meanAge;
 	private double drugRate;
-	private double employmentRate;
-	private double voteTurnout;
+	private double unemploymentRate;
+	private double incapacityBenefit;
 
 	private Emotion sentiment;
+	private double childInNoWorkHouse;
+	private double schoolAbscences;
+	private double GCSEScore;
+	private double deliberateFires;
+	private double incomeSupport;
+	
 	static Gson gson = new Gson();
 	
 	public SocialFactors(String wardCode) {
@@ -28,12 +31,15 @@ public class SocialFactors {
 		location.add(wardCode);
 		crimeRate = -1;
 		housePrice = -1;
-		educationRating = -1;
 		transportRating = -1;
-		meanAge = -1;
 		drugRate = -1;
-		employmentRate = -1;
-		voteTurnout = -1;
+		unemploymentRate = -1;
+		incapacityBenefit = -1;
+		childInNoWorkHouse = -1;
+		schoolAbscences = -1;
+		GCSEScore = -1;
+		deliberateFires = -1;
+		incomeSupport = -1;
 	}
 	
 	public static SocialFactors parseJSON(String location) {
@@ -44,12 +50,15 @@ public class SocialFactors {
 	public SocialFactors(DBObject readIn) {
 		crimeRate = (double) readIn.get("crimeRate");
 		housePrice = (double) readIn.get("housePrice");
-		educationRating = (double) readIn.get("educationRating");
 		transportRating = (double) readIn.get("transportRating");
-		meanAge = (double) readIn.get("meanAge");
 		drugRate = (double) readIn.get("drugRate");
-		employmentRate = (double) readIn.get("employmentRate");
-		voteTurnout = (double) readIn.get("voteTurnout");
+		unemploymentRate = (double) readIn.get("unemploymentRate");
+		incapacityBenefit = (double) readIn.get("incapacityBenefit");
+		childInNoWorkHouse = (double) readIn.get("childInNoWorkHouse");
+		schoolAbscences = (double) readIn.get("schoolAbscences");
+		GCSEScore = (double) readIn.get("GCSEScore");
+		deliberateFires = (double) readIn.get("deliberateFires");
+		incomeSupport = (double) readIn.get("incomeSupport");
 		
 		this.location = new ArrayList<String>();
 		BasicDBObject locations = (BasicDBObject) readIn.get("locations");
@@ -63,7 +72,6 @@ public class SocialFactors {
 	
 	public double getCrimeRate(){return crimeRate;}
 	public double getHousePrice(){return housePrice;}
-	public double getEducationRating(){return educationRating;}
 	public double getTransportRating(){return transportRating;}
 	
 	public ArrayList<String> getLocation(){return location;}
@@ -75,10 +83,6 @@ public class SocialFactors {
 	
 	public void setHousePrice(double housePrice){
 		this.housePrice = housePrice;
-	}
-	
-	public void setEducationRating(double educationRating) {
-		this.educationRating = educationRating;
 	}
 	
 	public void setTransportRating(double transportRating) {
@@ -93,12 +97,15 @@ public class SocialFactors {
 		switch (factorNumber) {
 		case 0: return this.crimeRate;
 		case 1: return this.housePrice;
-		case 2: return this.educationRating;
+		case 2: return this.GCSEScore;
 		case 3: return this.transportRating;
-		case 4: return this.meanAge;
+		case 4: return this.schoolAbscences;
 		case 5: return this.drugRate;
-		case 6: return this.employmentRate;
-		case 7: return this.voteTurnout;
+		case 6: return this.unemploymentRate;
+		case 7: return this.childInNoWorkHouse;
+		case 8: return this.deliberateFires;
+		case 9: return this.incapacityBenefit;
+		case 10: return this.incomeSupport;
 		default: return -1;
 		}
 	}
@@ -107,12 +114,15 @@ public class SocialFactors {
 		switch (factorNumber) {
 		case 0: return "crimeRate";
 		case 1: return "housePrice";
-		case 2: return "educationRating";
+		case 2: return "GCSEScore";
 		case 3: return "transportRating";
-		case 4: return "meanAge";
+		case 4: return "schoolAbscences";
 		case 5: return "drugRate";
-		case 6: return "employmentRate";
-		case 7: return "voteTurnout";
+		case 6: return "unemploymentRate";
+		case 7: return "childInNoWorkHouse";
+		case 8: return "deliberateFires";
+		case 9: return "incapacityBenefit";
+		case 10: return "incomeSupport";
 		default: return "";
 		}
 	}
@@ -132,25 +142,11 @@ public class SocialFactors {
 			housePrice = (housePrice + factors.getHousePrice())/2;
 		}
 		
-		if (educationRating == -1) {
-			educationRating = factors.getEducationRating();
-		}
-		else if (factors.getEducationRating()!= -1) {
-			educationRating = (educationRating + factors.getEducationRating())/2;
-		}
-		
 		if (transportRating == -1) {
 			transportRating = factors.getTransportRating();
 		}
 		else if (factors.getTransportRating() != -1) {
 			transportRating = (transportRating + factors.getTransportRating())/2;
-		}
-		
-		if (meanAge == -1) {
-			meanAge= factors.getMeanAge();
-		}
-		else if (factors.getMeanAge() != -1) {
-			meanAge = (meanAge+ factors.getMeanAge())/2;
 		}
 		
 		if (drugRate== -1) {
@@ -160,20 +156,55 @@ public class SocialFactors {
 			drugRate= (drugRate + factors.getDrugRate())/2;
 		}
 		
-		if (employmentRate == -1) {
-			employmentRate = factors.getEmploymentRate();
+		if (unemploymentRate == -1) {
+			unemploymentRate = factors.getUnemploymentRate();
 		}
-		else if (factors.getEmploymentRate() != -1) {
-			employmentRate = (employmentRate + factors.getEmploymentRate())/2;
+		else if (factors.getUnemploymentRate() != -1) {
+			unemploymentRate = (unemploymentRate + factors.getUnemploymentRate())/2;
 		}
 		
-		if (voteTurnout == -1) {
-			voteTurnout = factors.getVoteTurnout();
+		if (incomeSupport == -1) {
+			incomeSupport = factors.getIncomeSupport();
 		}
-		else if (factors.getVoteTurnout() != -1) {
-			voteTurnout= (voteTurnout+ factors.getVoteTurnout())/2;
+		else if (factors.getIncomeSupport() != -1) {
+			incomeSupport = (incomeSupport + factors.getIncomeSupport())/2;
+		}
+		
+		if (incapacityBenefit == -1) {
+			incapacityBenefit = factors.getIncapacityBenefit();
+		}
+		else if (factors.getIncapacityBenefit() != -1) {
+			incapacityBenefit = (incapacityBenefit + factors.getIncapacityBenefit())/2;
 		}
 
+		if (GCSEScore == -1) {
+			GCSEScore = factors.getGCSEScore();
+		}
+		else if (factors.getGCSEScore() != -1) {
+			GCSEScore = (GCSEScore + factors.getGCSEScore())/2;
+		}
+
+		if (childInNoWorkHouse == -1) {
+			childInNoWorkHouse = factors.getChildInNoWorkHouse();
+		}
+		else if (factors.getChildInNoWorkHouse() != -1) {
+			childInNoWorkHouse = (childInNoWorkHouse + factors.getChildInNoWorkHouse())/2;
+		}
+		
+		if (schoolAbscences == -1) {
+			schoolAbscences = factors.getSchoolAbscences();
+		}
+		else if (factors.getSchoolAbscences() != -1) {
+			schoolAbscences = (schoolAbscences + factors.getSchoolAbscences())/2;
+		}
+		
+		if (deliberateFires == -1) {
+			deliberateFires = factors.getDeliberateFires();
+		}
+		else if (factors.getDeliberateFires() != -1) {
+			deliberateFires = (deliberateFires + factors.getDeliberateFires())/2;
+		}
+		
 		//sentiment.averageSentiments(factors.getSentiment());
 		location.addAll(factors.getLocation());
 	}
@@ -187,39 +218,20 @@ public class SocialFactors {
 		}
 		
 		factors.append("locations", locations);
-		factors.append("educationRating", educationRating);
 		factors.append("transportRating", transportRating);
 		factors.append("housePrice", housePrice);
 		factors.append("crimeRate", crimeRate);
-		factors.append("meanAge", meanAge);
-		factors.append("employmentRate", employmentRate);
+		factors.append("unemploymentRate", unemploymentRate);
 		factors.append("drugRate", drugRate);
-		factors.append("voteTurnout", voteTurnout);
 		return factors;
 	}
 
-	public double getMeanAge() {
-		return meanAge;
+	public double getUnemploymentRate() {
+		return unemploymentRate;
 	}
 
-	public void setMeanAge(double meanAge) {
-		this.meanAge = meanAge;
-	}
-
-	public double getEmploymentRate() {
-		return employmentRate;
-	}
-
-	public void setEmploymentRate(double employmentRate) {
-		this.employmentRate = employmentRate;
-	}
-
-	public double getVoteTurnout() {
-		return voteTurnout;
-	}
-
-	public void setVoteTurnout(double voteTurnout) {
-		this.voteTurnout = voteTurnout;
+	public void setUnemploymentRate(double unemploymentRate) {
+		this.unemploymentRate = unemploymentRate;
 	}
 
 	public double getDrugRate() {
@@ -228,5 +240,64 @@ public class SocialFactors {
 
 	public void setDrugRate(double drugRate) {
 		this.drugRate = drugRate;
+	}
+
+	public void setIncapacityBenefit(double incapacityBenefit) {
+		this.incapacityBenefit = incapacityBenefit;
+		
+	}
+	
+	public double getIncapacityBenefit() {
+		return incapacityBenefit;
+	}
+
+	public void setIncomeSupport(double incomeSupport) {
+		this.incomeSupport = incomeSupport;
+		
+	}
+	
+	public double getIncomeSupport() {
+		return incomeSupport;
+		
+	}
+
+	public void setDeliberateFires(double deliberateFires) {
+		this.deliberateFires = deliberateFires;
+		
+	}
+	
+	public double getDeliberateFires() {
+		return deliberateFires;
+		
+	}
+
+	public double getGCSEScore() {
+		return GCSEScore;
+		
+	}
+	
+	public void setGCSEScore(double GCSEScore) {
+		this.GCSEScore = GCSEScore;
+		
+	}
+
+	public void setSchoolAbscences(double schoolAbscences) {
+		this.schoolAbscences = schoolAbscences;
+		
+	}
+
+	public double getSchoolAbscences() {
+		return schoolAbscences;
+		
+	}
+	
+	public void setChildInNoWorkHouse(double childInNoWorkHouse) {
+		this.childInNoWorkHouse = childInNoWorkHouse;
+		
+	}
+	
+	public double getChildInNoWorkHouse() {
+		return childInNoWorkHouse;
+		
 	}
 }
